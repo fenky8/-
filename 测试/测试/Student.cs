fc2527b89df2bd2d2d252081343fc97697acb45f
fenky <hzx1008@qq.com>
2019-01-10 11:15:11 +0800

    添加类

---
+++ 测试/测试/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 测试
+{
+    class Student
+    {
+        public int ID { get; set; }
+    }
+}
